8648d9b592cc39b6609cf72749f6df74f7f0f55e
SajiaAfrin <sajiaafrinmitul05@gmail.com>
2021-10-19 19:35:58 +0600

    day16

---
+++ Flutter-Course-Day-16/lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:demo_project/textfield_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Demo App",
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(
+      //   fontFamily: "Lobster"
+      // ),  
+      // home: HomePage()
+      // home: ProfileScreen(),
+      home: TextFieldScreen(),
+    ),
+  );
+}
+
+
+
+
