09d9ca05abdb20edcd2157f59f63da4d88a5d8c6
SajiaAfrin <sajiaafrinmitul05@gmail.com>
2022-01-03 00:50:13 +0600

    imagepickersignuploginsroragefirestore

---
+++ Flutter-Course-Day-32/firebase_test/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_test/screens/welcome/welcome.dart';
+import 'package:flutter/material.dart';
+Future<void> main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
---
+++ Flutter-Course-Day-32/firebase_test/lib/screens/welcome/welcome.dart
@@ -0,0 +1,79 @@
+import 'package:firebase_test/screens/welcome/signup.dart';
+import 'package:firebase_test/utils/app_color.dart';
+import 'package:firebase_test/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32),
+         decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/image/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+              Text(
+                "Test me", 
+                style: TextStyle(
+                  fontSize: 53, 
+                  fontWeight: FontWeight.w600,
+                  color: AppColor.primaryColor
+                ),
+              ),
+              Container(
+                width: 229,
+                height: 32,
+                decoration: BoxDecoration(
+                  color: AppColor.secondaryColor,
+                  borderRadius: BorderRadius.circular(5)
+                ),
+                child: Center(
+                  child: Text(
+                  "User Authentica", 
+                  style: TextStyle(
+                    fontSize: 19, 
+                    fontWeight: FontWeight.w500,
+                    color: AppColor.primaryColor
+                  ),
+               ),
+                ),
+              ),
+              SizedBox(height: 43,),
+              CustomButton(
+                onTap: (){
+                  Route route = MaterialPageRoute(builder: (ctx)=> LoginScreen());
+                  Navigator.push(context, route);
+                },
+                buttonLevel: "Login",
+                color: AppColor.secondaryColor,
+                levelColor: AppColor.primaryColor,
+              ),
+              SizedBox(height: 13,),
+              CustomButton(
+                onTap: (){
+                  Route route = MaterialPageRoute(builder: (ctx)=> SignUpScreen());
+                  Navigator.push(context, route);
+                },
+                buttonLevel: "Sign up",
+                color: AppColor.primaryColor,
+                levelColor: AppColor.secondaryColor,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
